f9499bd5a2ffbba71a44f39f9e111e3ff0920995
GengYu Rao <zouyoo@outlook.com>
2019-07-04 23:32:45 +0800

    fix up

---
+++ examples/hello/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo 'you wanna to convert ' $1
+#~/yoo/data/soc-flickr.mtx
+git pull 
+make clean
+make -j16
+./bin/test_hello_9* --graph-type market --graph-file $1 \
+        --quiet --quick 
+
+mv ./vertex.bin $1.vertex.yoo
+mv ./graph.bin $1.graph.yoo
