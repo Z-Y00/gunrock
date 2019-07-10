6478e81a4428b0672d05d4166aa864b25abddefe
GengYu Rao <zouyoo@outlook.com>
2019-07-10 23:12:22 +0800

    fix more

--- examples/rw/test.sh
+++ examples/rw/test.sh
@@ -1,6 +1,2 @@
-./bin/test_rw_* --graph-type market --graph-file ~/yoo/data/soc-BlogCatalog.mtx     --walk-mode 0 --seed 123 --store-walks 1  --quick  --walk-length 40 --num-runs 32
-./bin/test_rw_* --graph-type market --graph-file ~/yoo/data/soc-youtube.mtx     --walk-mode 0 --seed 123 --store-walks 1  --quick  --walk-length 40 --num-runs 32
-./bin/test_rw_* --graph-type market --graph-file ~/yoo/data/soc-flickr.mtx     --walk-mode 0 --seed 123 --store-walks 1  --quick  --walk-length 40 --num-runs 32
-./bin/test_rw_* --graph-type market --graph-file ~/yoo/data/soc-BlogCatalog.mtx     --walk-mode 0 --seed 123 --store-walks 1  --quick  --walk-length 40 --num-runs 32
-./bin/test_rw_* --graph-type market --graph-file ~/yoo/data/soc-sinaweibo.mtx     --walk-mode 0 --seed 123 --store-walks 1  --quick  --walk-length 40 --num-runs 32
-./bin/test_rw_* --graph-type market --graph-file ~/yoo/data/soc-twitter-2010.mtx --walk-mode 0 --seed 123 --store-walks 1  --quick  --walk-length 40 --num-runs 32
+#!/bin/bash
+./bin/test_rw_* --graph-type market --graph-file $1     --walk-mode 0 --seed 123 --store-walks 1  --quick  --walk-length 40 --num-runs 32
